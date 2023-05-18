9468bf8557de32847df4c27efdd64e4d9a61ca31
Zaid-maker <pzhafeez@gmail.com>
2023-05-18 19:16:56 +0500

    add initial main js file for the app

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,5 +4,45 @@
  * @copyright codewithsadee 2023
  */
 
+"use strict";
 
-'use strict';
+/**
+ *
+ * @param {NodeList} $elements
+ * @param {String} eventType
+ * @param {Function} callback
+ */
+const addEventOnElements = function ($elements, eventType, callback) {
+  for (const $item of $elements) {
+    $item.addEventListener(eventType, callback);
+  }
+};
+
+/**
+ * Header scroll state
+ */
+const $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  $header.classList[this.window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/**
+ * Search Toggle
+ */
+
+const $searchToggler = document.querySelector("[data-search-toggler]");
+
+const $searchField = document.querySelector("[data-search-field]");
+
+let isExpanded = false;
+
+$searchToggler.addEventListener("click", function () {
+  $header.classList.toggle("search-active");
+
+  isExpanded = isExpanded ? false : true;
+
+  this.setAttribute("aria-expanded", isExpanded);
+
+  $searchField.focus();
+});
